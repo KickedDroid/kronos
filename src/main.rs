9363eaeba99dc58dcde65d1e9eedd1a2563e18f7
Kicked Droid <24928676+KickedDroid@users.noreply.github.com>
2024-10-09 12:25:33 -0500

    Fixed
    Result check

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
+use clap::Parser;
 use cli_clipboard::{ClipboardContext, ClipboardProvider};
 use reqwest::header::HeaderMap;
 use serde::Deserialize;
-use tokio::fs;
 use std::collections::HashMap;
 use std::thread;
 use std::time::Duration;
-use clap::Parser;
 use std::time::SystemTime;
+use tokio::fs;
 const TARGET_PHRASE: &str = "27b47455f301788ebf9f85d0d1aa90d5";
 
 use reqwest::Client;
@@ -15,7 +15,7 @@ use serde_json::json;
 const API_URL: &str = "https://labs.hackthebox.com/";
 #[derive(Deserialize)]
 pub struct Config {
-    pub htb: HashMap<String, String>
+    pub htb: HashMap<String, String>,
 }
 
 #[derive(Parser, Debug)]
@@ -31,15 +31,17 @@ async fn main() {
 
     let args = Args::parse();
 
-    let toml_string = fs::read_to_string("config.toml").await.expect("No config.toml file found");
-    let mut config: Config = toml::from_str(toml_string.as_str()).expect("Failed to read config.toml");
+    let toml_string = fs::read_to_string("config.toml")
+        .await
+        .expect("No config.toml file found");
+    let mut config: Config =
+        toml::from_str(toml_string.as_str()).expect("Failed to read config.toml");
 
     let mut auto = true;
     if args.disable_auto {
         auto = false;
     }
 
-    
     let mut ctx = ClipboardContext::new().expect("Error reading clipboard");
     let mut last_content = String::new();
     match now.elapsed() {
@@ -49,7 +51,7 @@ async fn main() {
     println!("Daemon started. Listening for flags");
 
     loop {
-        thread::sleep(Duration::from_millis(100));
+        thread::sleep(Duration::from_secs(1));
 
         let content = match ctx.get_contents() {
             Ok(content) => content,
@@ -75,23 +77,25 @@ async fn main() {
                 }
             }
             if is_valid_hex(&content) && !auto {
-                use std::io::{stdin,stdout,Write};
+                use std::io::{stdin, stdout, Write};
                 let mut s = String::new();
                 print!("Please confirm you want to send flag: y/n\n");
-                let _=stdout().flush();
-                stdin().read_line(&mut s).expect("Please Enter a valid string");
+                let _ = stdout().flush();
+                stdin()
+                    .read_line(&mut s)
+                    .expect("Please Enter a valid string");
                 if let Some('y') = s.chars().next_back() {
                     println!("Sending Flag");
-                let result = submit_flag(&content, &config).await;
-                if result {
-                    println!("Congrats the flag was submited!!");
-                    match now.elapsed() {
-                        Ok(time) => println!("Time: {}", time.as_secs() / 60 / 60),
-                        Err(e) => eprintln!("SHit {}", e),
+                    let result = submit_flag(&content, &config).await;
+                    if result {
+                        println!("Congrats the flag was submited!!");
+                        match now.elapsed() {
+                            Ok(time) => println!("Time: {}", time.as_secs() / 60 / 60),
+                            Err(e) => eprintln!("SHit {}", e),
+                        }
+                    } else {
+                        println!("Sorry Wrong Flag :( or something happened, try submitting the flag and reporting to me about what happened.")
                     }
-                } else {
-                    println!("Sorry Wrong Flag :( or something happened, try submitting the flag and reporting to me about what happened.")
-                }
                 } else if s.contains("n") {
                     println!("Skipping contents, not sending flag.")
                 }
@@ -110,7 +114,6 @@ fn is_valid_hex(s: &str) -> bool {
     true
 }
 
-
 async fn submit_flag(flag: &str, config: &Config) -> bool {
     let client = Client::new();
     let url = format!("{}api/v4/arena/own", API_URL);
@@ -118,10 +121,20 @@ async fn submit_flag(flag: &str, config: &Config) -> bool {
         "flag": flag,
     });
 
-
     let mut headers = HeaderMap::new();
-    headers.insert("Authorization", format!("Bearer {}", config.htb["api_token"]).parse().expect("Failed to read config.toml"));
-    headers.insert("Content-Type", "application/json".to_string().parse().expect("Failed to parse Content-Type"));
+    headers.insert(
+        "Authorization",
+        format!("Bearer {}", config.htb["api_token"])
+            .parse()
+            .expect("Failed to read config.toml"),
+    );
+    headers.insert(
+        "Content-Type",
+        "application/json"
+            .to_string()
+            .parse()
+            .expect("Failed to parse Content-Type"),
+    );
 
     let response = client
         .post(&url)
@@ -134,7 +147,7 @@ async fn submit_flag(flag: &str, config: &Config) -> bool {
         Ok(res) => {
             if let Ok(text) = res.text().await {
                 println!("{}", text);
-                text.contains("is now owned.")
+                text.contains("owned.")
             } else {
                 false
             }
