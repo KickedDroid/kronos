4f9d57620e01b8aef1c91c3291e52a3717b85570
Kicked Droid <24928676+KickedDroid@users.noreply.github.com>
2024-10-15 14:59:40 -0500

    arboord

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
+use arboard::{Clipboard, Get};
 use clap::Parser;
-use cli_clipboard::{ClipboardContext, ClipboardProvider};
 use reqwest::header::HeaderMap;
 use serde::Deserialize;
 use std::collections::HashMap;
@@ -7,7 +7,7 @@ use std::thread;
 use std::time::Duration;
 use std::time::SystemTime;
 use tokio::fs;
-const TARGET_PHRASE: &str = "27b47455f301788ebf9f85d0d1aa90d5";
+const TARGET_STRING: &str = "27b47455f301788ebf9f85d0d1aa90d5";
 
 use reqwest::Client;
 use serde_json::json;
@@ -30,62 +30,57 @@ async fn main() {
     let now = SystemTime::now();
 
     let args = Args::parse();
+    let _session_type = session_type();
+    //assert!(session_type == SessionType::Wayland);
 
     let toml_string = fs::read_to_string("config.toml")
         .await
-        .expect("No config.toml file found");
+        .expect("[-] No config.toml file found");
     let mut config: Config =
-        toml::from_str(toml_string.as_str()).expect("Failed to read config.toml");
+        toml::from_str(toml_string.as_str()).expect("[+] Failed to read config.toml");
 
     let mut auto = true;
     if args.disable_auto {
         auto = false;
     }
 
-    let mut ctx = ClipboardContext::new().expect("Error reading clipboard");
+    let mut ctx = Clipboard::new().expect("[-] Error reading clipboard");
     let mut last_content = String::new();
     match now.elapsed() {
-        Ok(time) => println!("Time: {}", time.as_secs() / 60 / 60),
+        Ok(time) => println!("[+] Time: {}", time.as_secs() / 60 / 60),
         Err(e) => eprintln!("SHit {}", e),
     }
-    println!("Daemon started. Listening for flags");
+    println!("[+] Daemon started. Listening for flags");
 
     loop {
+        // Bounds
+        match now.elapsed() {
+            Ok(time) => assert!(time.as_secs() < 86400),
+            Err(_) => break,
+        }
+        // Itnerval (No bennefit from lowering the Duration)
         thread::sleep(Duration::from_secs(1));
 
-        let content = match ctx.get_contents() {
+        let content = match ctx.get_text() {
             Ok(content) => content,
             Err(_) => continue,
         };
         if content.contains(" ") {
             continue;
         }
+        if content.len() != TARGET_STRING.len() {
+            continue;
+        }
+        assert!(content.len() == TARGET_STRING.len());
 
-        if content.len() == TARGET_PHRASE.len() && content != last_content {
+        let valid = is_valid_hex(&content);
+
+        if valid && content != last_content {
             last_content = content.clone();
-            if is_valid_hex(&content) && auto {
-                println!("Sending Flag");
-                let result = submit_flag(&content, &config).await;
-                if result {
-                    println!("Congrats the flag was submited!!");
-                    match now.elapsed() {
-                        Ok(time) => println!("Time: {}", time.as_secs() / 60 / 60),
-                        Err(e) => eprintln!("SHit {}", e),
-                    }
-                } else {
-                    println!("Sorry Wrong Flag :( or something happened, try submitting the flag and reporting to me about what happened.")
-                }
-            }
-            if is_valid_hex(&content) && !auto {
-                use std::io::{stdin, stdout, Write};
-                let mut s = String::new();
-                print!("Please confirm you want to send flag: y/n\n");
-                let _ = stdout().flush();
-                stdin()
-                    .read_line(&mut s)
-                    .expect("Please Enter a valid string");
-                if let Some('y') = s.chars().next_back() {
-                    println!("Sending Flag");
+
+            match auto {
+                true => {
+                    println!("[+] Sending Flag");
                     let result = submit_flag(&content, &config).await;
                     if result {
                         println!("Congrats the flag was submited!!");
@@ -94,13 +89,60 @@ async fn main() {
                             Err(e) => eprintln!("SHit {}", e),
                         }
                     } else {
-                        println!("Sorry Wrong Flag :( or something happened, try submitting the flag and reporting to me about what happened.")
+                        println!("[-] Sorry Wrong Flag :( or something happened, try submitting the flag and reporting to me about what happened.")
                     }
-                } else if s.contains("n") {
-                    println!("Skipping contents, not sending flag.")
+                }
+                false => {
+                    use std::io::{stdin, stdout, Write};
+                    let mut s = String::new();
+                    print!("Please confirm you want to send flag: y/n\n");
+                    let _ = stdout().flush();
+                    stdin()
+                        .read_line(&mut s)
+                        .expect("Please Enter a valid string");
+                    match s.chars().next_back() {
+                        Some('y') => {
+                            println!("[+] Sending Flag");
+                            let result = submit_flag(&content, &config).await;
+                            if result {
+                                println!("Congrats the flag was submited!!");
+                                match now.elapsed() {
+                                    Ok(time) => println!("[+] Time: {}", time.as_secs() / 60 / 60),
+                                    Err(e) => eprintln!("SHit {}", e),
+                                }
+                            } else {
+                                println!("[-] Sorry Wrong Flag :( or something happened, try submitting the flag and reporting to me about what happened.")
+                            }
+                        }
+                        Some('n') => println!("Ignoring conntents"),
+                        _ => println!("y/n only"),
+                    }
                 }
             }
         }
+    }
+}
+#[derive(PartialEq)]
+enum SessionType {
+    X11,
+    Wayland,
+    Unknown,
+    FAILED,
+}
+
+fn session_type() -> SessionType {
+    if let Ok(session_type) = std::env::var("XDG_SESSION_TYPE") {
+        match session_type.as_str() {
+            "x11" => SessionType::X11,
+            "wayland" => {
+                println!("[+] Running on Wayland.");
+                SessionType::Wayland
+            }
+            _ => SessionType::Unknown,
+        }
+    } else {
+        println!("[-] Could not read XDG_SESSION_TYPE");
+        SessionType::FAILED
     }
 }
 
@@ -126,14 +168,14 @@ async fn submit_flag(flag: &str, config: &Config) -> bool {
         "Authorization",
         format!("Bearer {}", config.htb["api_token"])
             .parse()
-            .expect("Failed to read config.toml"),
+            .expect("[-] Token Error"),
     );
     headers.insert(
         "Content-Type",
         "application/json"
             .to_string()
             .parse()
-            .expect("Failed to parse Content-Type"),
+            .expect("[-] Failed to parse Content-Type"),
     );
 
     let response = client
@@ -147,7 +189,7 @@ async fn submit_flag(flag: &str, config: &Config) -> bool {
         Ok(res) => {
             if let Ok(text) = res.text().await {
                 println!("{}", text);
-                text.contains("pwned.")
+                text.contains("pwned")
             } else {
                 false
             }
